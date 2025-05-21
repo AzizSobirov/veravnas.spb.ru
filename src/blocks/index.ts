23a9cd755a6666b804e992df24721a11cd0ea652
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2025-05-21 22:22:29 +0500

    Add About and Specialists sections with corresponding styles and assets
    - Introduced a new SVG icon for the play button.
    - Created the About section with video and text content, including a button for more information.
    - Added styles for the About section to ensure responsive design.
    - Created a placeholder for the Specialists section with basic structure and styles.

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,6 +1,8 @@
+export { default as About } from "./About.astro";
 export { default as Application } from "./Application.astro";
 export { default as Callback } from "./Callback.astro";
 export { default as Directions } from "./Directions.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as Path } from "./Path.astro";
+export { default as Specialists } from "./Specialists.astro";
 export { default as Trust } from "./Trust.astro";
